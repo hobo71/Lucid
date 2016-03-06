57aa03123215e17a9f9f7e24c6af57e81e9fb28d
Adam Murray <adam.john.campbell.murray@gmail.com>
2016-03-06 23:19:04 +0000

    Add documentation

--- gulpfile.js
+++ gulpfile.js
@@ -4,33 +4,40 @@
 // Base setup
 var gulp    = require('gulp'),
     sass    = require('gulp-sass'),
-    concat  = require('gulp-concat');
+    concat  = require('gulp-concat'),
+    sassdoc = require('sassdoc');
 
 // Folders
 var src = 'src/';
 
 // Concatenate .scss files
 gulp.task('sass', function() {
-  gulp.src(src + '*.scss')
+  gulp.src(src + '**/*.scss')
       .pipe(concat('library.scss'))
       .pipe(gulp.dest(''));
 });
 
 // Compile and concatenate .scss files
 gulp.task('sass-to-css', function() {
-  gulp.src(src + '*.scss')
+  gulp.src(src + '**/*.scss')
       .pipe(sass())
       .pipe(concat('library.css'))
       .pipe(gulp.dest(''));
+});
+
+// Sassdoc
+gulp.task('sassdocs', function () {
+  return gulp.src(src + '**/*.scss')
+    .pipe(sassdoc());
 });
 
 // Set up watcher
 gulp.task('watch', function() {
 
   // Watch .scss files
-  gulp.watch(src + '*.scss', ['sass', 'sass-to-css']);
+  gulp.watch(src + '*.scss', ['sass', 'sass-to-css', 'sassdocs']);
 
 });
 
 // Default task
-gulp.task('default', ['sass', 'sass-to-css', 'watch']);
+gulp.task('default', ['sass', 'sass-to-css', 'sassdocs', 'watch']);
